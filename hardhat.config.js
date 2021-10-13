2e1647f3d93c5a29a35f9a136902f67639f7d2e8
nmlml99 <moore.token.official@example.com>
2021-10-13 01:05:44 -0700

    n

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,37 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privatekey = fs.readFileSync(".env").toString().trim();
 
 module.exports = {
-  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
-    },
-    /*
-    mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
-    },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
-    }
-    */
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    }, 
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [privatekey]
       }
-    }
-  }
-};
-
+    }, 
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [privatekey]
+},
+  solidity: "0.8.4"
+}
